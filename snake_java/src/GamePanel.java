815f06dd55b693e83656fb4ff70df87e9ec13c7a
egorakulov <eakulov@uw.edu>
2024-11-15 19:57:27 -0800

    finishes pong and snake

--- snake_java/src/GamePanel.java
+++ snake_java/src/GamePanel.java
@@ -10,7 +10,7 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
-    static final int DELAY = 60;
+    static final int DELAY = 85;
     final int[] x = new int[GAME_UNITS];
     final int[] y = new int[GAME_UNITS];
     int bodyParts = 4;
