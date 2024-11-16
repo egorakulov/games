c2f1275d4e184ce83e9072c9ac5ef2b6c1927da2
egorakulov <eakulov@uw.edu>
2024-11-15 18:42:29 -0800

    fixes bug where ball can go through parts of paddle

--- pong_java/src/GamePanel.java
+++ pong_java/src/GamePanel.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 import java.util.concurrent.TimeUnit;
 
 public class GamePanel extends JPanel implements ActionListener{
-    static final int SCREEN_WIDTH = 1200;
+    static final int SCREEN_WIDTH = 1000;
     static final int SCREEN_HEIGHT = 600;
     static final int MOVEMENT_SPEED = 5;
     static final int PADDLE_HEIGHT = 75;
@@ -14,7 +14,7 @@ public class GamePanel extends JPanel implements ActionListener{
     private static Player p2;
     private static Ball b;
     static final int BALL_SIZE = 25;
-    static final int BALL_SPEED = 5;
+    static final int BALL_SPEED = 8;
     boolean running = false;
     Timer timer;
 
@@ -113,7 +113,8 @@ public class GamePanel extends JPanel implements ActionListener{
     public void checkGoal() throws InterruptedException {
         // bounce off left side
         if (b.x < PADDLE_WIDTH && b.x > 0
-                && p1.getyPosition() < b.y && b.y < p1.getyPosition() + PADDLE_HEIGHT) {
+                && p1.getyPosition() < b.y + (BALL_SIZE / 2)
+                && b.y + (BALL_SIZE / 2) < p1.getyPosition() + PADDLE_HEIGHT) {
             b.xDir = 'R';
         } else if ( b.x < 0) {
             Thread.sleep(500);
@@ -124,7 +125,8 @@ public class GamePanel extends JPanel implements ActionListener{
         // bounce off right side
         if (b.x > SCREEN_WIDTH - BALL_SIZE - PADDLE_WIDTH &&
                 b.x < SCREEN_WIDTH - BALL_SIZE &&
-                p2.getyPosition() < b.y && b.y < p2.getyPosition() + PADDLE_HEIGHT) {
+                p2.getyPosition() < b.y + (BALL_SIZE / 2)
+                && b.y + (BALL_SIZE / 2) < p2.getyPosition() + PADDLE_HEIGHT) {
             b.xDir = 'L';
         } else if ( b.x > SCREEN_WIDTH - BALL_SIZE) {
             Thread.sleep(500);
@@ -134,7 +136,6 @@ public class GamePanel extends JPanel implements ActionListener{
     }
 
     public void resetBall(char loser) {
-        b = new Ball((SCREEN_WIDTH - BALL_SIZE) / 2, (SCREEN_HEIGHT - BALL_SIZE) / 2);
         b.x = (SCREEN_WIDTH - BALL_SIZE) / 2;
         b.y = (SCREEN_HEIGHT - BALL_SIZE) / 2;
         b.xDir = loser;
